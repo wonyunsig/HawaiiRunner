fb2b8a4bf7fa703e0b98c594543c2475228bf13c
woojin0605 <wonyunsig@gmail.com>
2023-07-04 11:50:39 +0900

    Add Player Jump

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     private Rigidbody2D rigid;
     private SpriteRenderer spriteRenderer;
     private Animator anim;
@@ -19,12 +20,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        //Jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping")) 
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
         //Stop Speed
-        if (Input.GetButtonUp("Horizontal"))
+    if (Input.GetButtonUp("Horizontal"))
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x*0.5f, rigid.velocity.y);
         }
-        
+
         //Direction Sprite
         if (Input.GetButtonDown("Horizontal"))
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
@@ -47,5 +55,20 @@ public class PlayerMovement : MonoBehaviour
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < maxSpeed * (-1))
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        
+        //Landing Platform
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if(rayHit.distance < 0.5f)
+                    anim.SetBool("isJumping",false);
+            }
+
+        }
     }
 }
